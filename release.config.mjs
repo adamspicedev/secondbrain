6a43ecff6a75ce223f7c5fb641ba240112900963
Adam Spice <adam@spicey.dev>
2026-06-21 16:08:33 +1200

    Add release rules to commit analyzer for improved versioning

--- release.config.mjs
+++ release.config.mjs
@@ -5,7 +5,19 @@ export default {
     [
       "@semantic-release/commit-analyzer",
       {
-        preset: "conventionalcommits"
+        preset: "conventionalcommits",
+        releaseRules: [
+          { subject: "Feat/*", release: "minor" },
+          { subject: "feat/*", release: "minor" },
+          { subject: "Add *", release: "minor" },
+          { subject: "add *", release: "minor" },
+          { subject: "Fix *", release: "patch" },
+          { subject: "fix *", release: "patch" },
+          { subject: "Refactor *", release: "patch" },
+          { subject: "refactor *", release: "patch" },
+          { subject: "Update *", release: "patch" },
+          { subject: "update *", release: "patch" }
+        ]
       }
     ],
     [
